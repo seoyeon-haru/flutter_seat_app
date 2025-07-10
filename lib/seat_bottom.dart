cf281f6936bdd3e74d8b8a441bc6789dbaef0c96
seoyeon-haru <sy5792@naver.com>
2025-07-10 21:40:16 +0900

    Adjust SeatBottom size

--- lib/seat_bottom.dart
+++ lib/seat_bottom.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 class SeatBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  return Expanded(child: Container(
+  return Container(
+    height: 200,
     color: Colors.blue,
-  ),);
+  );
   }
 
 }
