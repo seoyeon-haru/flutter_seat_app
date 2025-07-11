e045f878d0603aefd65df633b0e7696faec97251
seoyeon-haru <sy5792@naver.com>
2025-07-11 17:58:37 +0900

    Wrap content in Container with top rounded corners

--- lib/seat_bottom.dart
+++ lib/seat_bottom.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 class SeatBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  return Container(
-    height: 200,
-    color: Colors.blue,
-  );
+    return Container(
+      height: 200,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          )),
+    );
   }
-
 }
